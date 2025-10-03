5cc6973f0cc87c6900c77f527bced73ad9aee483
Roman Castro <xxromanxxtm@gmail.com>
2025-10-03 08:50:10 -0300

    outline red

--- app/helpers/apiCall.js
+++ app/helpers/apiCall.js
@@ -1,6 +1,9 @@
 export async function apiCall(endpoint, method = 'GET', data = null) {
+
   const options = { method, headers: { 'Content-Type': 'application/json' } };
   if (data) options.body = JSON.stringify(data);
-  const res = await fetch('https://c2871218.ferozo.com/server/api.php/' + endpoint, options);
+  const res = await fetch('http://streamlab.com.ar/server/api.php/' + endpoint, options);
+  console.log(endpoint, options);
+
   return await res.json();
 }
