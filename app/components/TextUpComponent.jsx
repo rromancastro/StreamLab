3533941ae1c7c879dfed255242c23926201f2845
Roman Castro <xxromanxxtm@gmail.com>
2025-10-09 17:06:46 -0300

    detalles

--- app/components/TextUpComponent.jsx
+++ app/components/TextUpComponent.jsx
@@ -1,6 +1,6 @@
 
 export const TextUpComponent = ({fontSize, textContent, active, color = '#000'}) => {
     return <div className="textUpComponent" style={{height: fontSize + 10}}>
-                <p style={{transform: active ? 'translateY(15px)' : `translateY(${Number(fontSize) + 20}px)`, fontSize: `${fontSize}px`, color: color}}>{textContent}</p>
+                <p style={{transform: active ? 'translateY(15px)' : `translateY(${Number(fontSize) + 25}px)`, fontSize: `${fontSize}px`, color: color}}>{textContent}</p>
         </div>
 }
